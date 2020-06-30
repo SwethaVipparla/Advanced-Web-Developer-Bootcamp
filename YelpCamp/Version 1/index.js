74dc23333208da1b96cdb1da9194026cb8b5a61e
Swetha Vipparla <65361205+SwethaVipparla@users.noreply.github.com>
2020-06-30 14:16:44 +0400

    Create index.js

---
+++ YelpCamp/Version 1/index.js
@@ -0,0 +1,38 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+var campgrounds = [
+		{name: "Salmon Creek", image: "https://www.nps.gov/lacl/planyourvisit/images/Image-w-cred-cap_-1200w-_-Visit-Silver-Salmon-Creek-Page_2.jpg?maxwidth=1200&maxheight=1200&autorotate=false"},
+		{name: "Granite Hill", image: "https://live.staticflickr.com/3543/3297277787_67de0b1db9_b.jpg"},
+		{name: "Mountain Goat's Rest", image: "https://www.mountainphotography.com/images/xl/20120826-Campsite-Goats.jpg"}
+	]
+
+app.get("/", function(req, res){
+	res.render("landing");
+});
+
+app.get("/campgrounds", function(req, res){
+	res.render("campgrounds", {campgrounds: campgrounds});
+});
+
+app.post("/campgrounds", function(req, res){
+	//get data from form and add to campgrounds array
+	var name = req.body.name;
+	var image = req.body.image;
+	var newCampground = {name: name, image: image};
+	campgrounds.push(newCampground);
+	//redirect back to campgrounds page
+	res.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new", function(req, res){
+	res.render("new.ejs");
+});
+
+app.listen(3000, function() { 
+  console.log('The YelpCamp Server has started!'); 
+});
